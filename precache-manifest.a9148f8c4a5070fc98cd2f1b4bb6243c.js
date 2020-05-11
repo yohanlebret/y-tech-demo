f3fbc28940468f61ebdd9131118c1807b003bd59
Yohan Lebret <yohan.lebret@gmail.com>
2020-05-11 19:43:30 +0100

    Updates

---
+++ precache-manifest.a9148f8c4a5070fc98cd2f1b4bb6243c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8bcd2df73ca716645ce32ee9b086f54",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8ffd436be324d968b2a5",
+    "url": "/static/js/2.4bfb10c7.chunk.js"
+  },
+  {
+    "revision": "0ac79d53dda6ffa0c6325c90109848cb",
+    "url": "/static/js/2.4bfb10c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55c2a5b37a86349f667c",
+    "url": "/static/js/main.a1a8997e.chunk.js"
+  },
+  {
+    "revision": "39fd01b85965c045f5dc",
+    "url": "/static/js/runtime-main.5b6c2d2e.js"
+  }
+]);
