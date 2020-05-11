70e98cf0fee87995f09827511254e30a7ef4d817
Yohan Lebret <yohan.lebret@gmail.com>
2020-05-11 19:53:07 +0100

    Updates

---
+++ precache-manifest.7aa73abc05950faf1409e48728787b28.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96a7eb8f6d44ced1258624d5f229961e",
+    "url": "/y-tech-demo/index.html"
+  },
+  {
+    "revision": "8ffd436be324d968b2a5",
+    "url": "/y-tech-demo/static/js/2.4bfb10c7.chunk.js"
+  },
+  {
+    "revision": "0ac79d53dda6ffa0c6325c90109848cb",
+    "url": "/y-tech-demo/static/js/2.4bfb10c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d9299152e6f3ce97c62",
+    "url": "/y-tech-demo/static/js/main.f083a4cc.chunk.js"
+  },
+  {
+    "revision": "9dc6cf74884731e047b7",
+    "url": "/y-tech-demo/static/js/runtime-main.f544d36b.js"
+  }
+]);
